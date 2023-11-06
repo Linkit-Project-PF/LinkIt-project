77377f2bdfbc117a4f5759a6b1c705db44b032fc
Jose Lozano <joselozano.dev@gmail.com>
2023-11-06 18:17:21 -0400

    Add new client and technology logos, update Tailwind config and index.html

--- linkit-project-client/tailwind.config.js
+++ linkit-project-client/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        linkIt:{
+          "50": "#CBDAE8",
+         "100": "#A9C1D9",
+         "200": "#173951",
+         "300": "#019A83",
+         "400": "#2E2D2C"
+        }
+      },
+      fontFamily:{
+        montserrat: ["Montserrat", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
