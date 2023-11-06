08ae76362e012efc2e55b58bedb396b766052fd6
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-06 12:54:42 -0500

    Creating Routes

--- linkit-project-client/src/App.tsx
+++ linkit-project-client/src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <Routes>
+        <NavBar>La navBar XD</NavBar>
+        <Route path='/' element={<Home />} />
+        <Route path='/soyEmpresa' element={<SoyEmpresa />} />
+        <Route path='/soyTalento' element={<SoyTalento />} />
+        <Route path='/recursos' element={<Recursos />} />
+        <Route path='/quienesSomos' element={<QuienesSomos />} />
+      </Routes>
     </>
   )
 }
