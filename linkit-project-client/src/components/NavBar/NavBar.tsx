274893d2dbaed3ff48b4361d42b9ec8b69504778
jimenzedwar <jimenzeddi@gmail.com>
2023-11-07 16:42:37 -0500

    Add styling and menu animation to NavBar component

--- linkit-project-client/src/components/NavBar/NavBar.tsx
+++ linkit-project-client/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,60 @@
 import "./NavBar.css"
 import Logo from "/Linkit-logo/linkit-logos-web_4-logo-horizontal-azul.svg"
 import { NavLink, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { useAnimate, stagger, motion } from "framer-motion";
+
+const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+
+function useMenuAnimation(isOpen: boolean) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+
+    animate(
+      "ul",
+      {
+        clipPath: isOpen
+          ? "inset(0% 0% 0% 0% round 10px)"
+          : "inset(10% 50% 90% 50% round 10px)"
+      },
+      {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5
+      }
+    );
+
+    animate(
+      "li",
+      isOpen
+        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
+        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
+      {
+        duration: 0.2,
+        delay: isOpen ? staggerMenuItems : 0
+      }
+    );
+  }, [isOpen]);
+
+  return scope;
+}
 
 function NavBar() {
     const navigate = useNavigate()
 
+    const [isOpenEmpresa, setIsOpenEmpresa] = useState(false);
+    const [isOpenRecursos, setIsOpenRecursos] = useState(false);
+    const [isOpenQS, setIsOpenQS] = useState(false);
+
+    const scopeQS = useMenuAnimation(isOpenQS);
+    const scopeRecursos = useMenuAnimation(isOpenRecursos);
+    const scopeEmpresa = useMenuAnimation(isOpenEmpresa);
+
+
+    const goHome = () => {
+        navigate("/")
+    }
     const goSoyEmpresa = () => {
         navigate("/SoyEmpresa")
     }
@@ -15,19 +65,83 @@ function NavBar() {
         <div>
         <div className="preNavbar">
             <span className="">Contrata y gestiona talentos de forma global con LinkIt | </span>
-            <span className="ml-2">Comienza ahora! →</span>
+            <NavLink to="https://calendly.com/linkit-project-henry/30min" className="ml-2">Comienza ahora! →</NavLink>
             </div>
-        <nav className="sticky top-0 bg-linkIt-50 h-[63px] flex">
-        <img src={Logo} alt="" className="relative bottom-12 h-40 left-24"/>
-        <div className="flex absolute right-44 top-2 items-center justify-center space-x-20 text-xl font-montserrat font-semibold">
-        <NavLink className="hover:border-b-4 hover:border-linkIt-300" to='/'>Home</NavLink>
-        <NavLink className="hover:border-b-4 hover:border-linkIt-300" to='/soyEmpresa'>Soy Empresa</NavLink>
+        <nav className="navBar">
+        <img src={Logo} alt="" className="h-40"/>
+        <div className="containerBtnsNavBar">
+        <motion.button onClick={()=> goHome()}>Home</motion.button>
+        <nav className="relative top-10" ref={scopeEmpresa}>
+        <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpenEmpresa(!isOpenEmpresa)}
+      >
+        Soy Empresa
+        <div className="arrow" style={{ transformOrigin: "50% 55%" }}>
+        </div>
+      </motion.button>
+      <ul
+        style={{
+          pointerEvents: isOpenEmpresa ? "auto" : "none",
+          clipPath: "inset(10% 50% 90% 50% round 10px)"
+        }}
+      >
+        <li>Servicios </li>
+        <li>Proceso </li>
+        <li>Cotiza </li>
+      </ul>{" "}
+      </nav>
+
+
         <NavLink className="hover:border-b-4 hover:border-linkIt-300" to='/soyTalento'>Soy Talento</NavLink>
-        <NavLink className="hover:border-b-4 hover:border-linkIt-300" to='/recursos'>Recursos</NavLink>
-        <NavLink className="hover:border-b-4 hover:border-linkIt-300" to='/quienesSomos'>Quiénes Somos</NavLink>
+
+
+        <nav className="relative top-10" ref={scopeRecursos}>
+        <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpenRecursos(!isOpenRecursos)}
+      >
+        Recursos
+      </motion.button>
+      <ul
+        style={{
+          pointerEvents: isOpenRecursos ? "auto" : "none",
+          clipPath: "inset(10% 50% 90% 50% round 10px)"
+        }}
+      >
+        <li>Blogs</li>
+        <li>Ebooks</li>
+        <li>Eventos</li>
+      </ul>{" "}
+
+
+      </nav>
+        <nav className="relative top-20" ref={scopeQS}>
+        <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpenQS(!isOpenQS)}
+      >
+        Quienes Somos
+        <div className="relative left-44 bottom-6">
+          <svg width="15" height="15" viewBox="0 0 20 20">
+            <path d="M0 7 L 20 7 L 10 16" />
+          </svg>
+        </div>
+      </motion.button>
+      <ul className="bg-white flex flex-col"
+        style={{
+          pointerEvents: isOpenQS ? "auto" : "none",
+        }}
+      >
+        <li className="border-gray border-b-2 mb-2">Mision</li>
+        <li className="border-gray border-b-2 mb-2">Vision</li>
+        <li className="border-gray border-b-2 mb-2">Valores e Historia</li>
+        <li>Talento interno</li>
+      </ul>{" "}
+      </nav>
         <span className=" font-medium">Inglés</span>
-        <button className="bg-linkIt-300 rounded-lg p-2 text-white font-medium shadow-md active:translate-y-1" onClick={()=> goSoyEmpresa()}>Contrata Talento</button>
-        <button className="border-linkIt-300 border rounded-lg p-2 font-medium shadow-sm active:translate-y-1" onClick={()=> goSoyTalento()}>Vacantes disponibles</button>
+        <motion.button className="bg-linkIt-300 rounded-lg p-2 h-10 text-white font-medium shadow-md" onClick={()=> goSoyEmpresa()} whileTap={{ scale: 0.9}}>Contrata Talento</motion.button>
+        <motion.button className="border-linkIt-300 border rounded-lg p-2 h-10 font-medium shadow-sm active:translate-y-1" onClick={()=> goSoyTalento()} whileTap={{ scale: 0.9}}>Vacantes disponibles</motion.button>
         </div>
         </nav>
 </div>
