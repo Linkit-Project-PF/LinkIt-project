7fdb00f0310cccaa00373d61f4cb132b38ea60ec
Jose Lozano <joselozano.dev@gmail.com>
2023-11-06 23:01:30 -0400

    Add animation and update Home page title

--- linkit-project-client/src/components/Home/Home.tsx
+++ linkit-project-client/src/components/Home/Home.tsx
@@ -1,6 +1,25 @@
+import { motion, Variants } from 'framer-motion'
+
+const divVariants: Variants = {
+    hidden: {
+        y: 300
+    },
+    visible: {
+        y: 0,
+        transition: {
+            duration: 0.5,
+            bounce: 0.3,
+        }
+    }
+}
+
 function Home() {
     return (
-        <h1>Esto es Home</h1>
+        <>
+        <h1>Home Page</h1>
+
+        </>
+        
     )
 }
 
