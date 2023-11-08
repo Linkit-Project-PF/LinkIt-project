71e7095db21269521d0e404cc4e6eef1beadb720
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-08 05:53:19 -0500

    Fixing buttons Module B  and adding numbers in module C from Home

--- linkit-project-client/src/components/Home/Modulos/ModuloC/ModuloC.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloC/ModuloC.tsx
@@ -7,19 +7,31 @@ export default function ModuloC() {
                     <div className="flex  flex-col items-center mx-12">
                         <li className="font-bold text-xl m-6">Sin riesgos</li>
                         <p className="text-xl text-center ">Ofrecemos un servicio de calidad a éxito. No hay anticipos; el pago es al efectivizar la contratación.</p>
+                        <br />
                     </div>
                     <div className="flex flex-col items-center mx-12">
                         <li className="font-bold text-xl m-6">Fee a medida</li>
                         <p className=" text-xl text-center">Personalizamos nuestro fee a tus necesidades y ahorra hasta un 50% en la contratación.</p>
+                        <br />
+
                     </div>
                     <div className="flex flex-col items-center mx-12">
                         <li className="font-bold text-xl m-6">Garantía</li>
                         <p className="text-xl  text-center">Garantía de por vida contratando a través de LinkIT.</p>
+                        <br />
+
                     </div>
                     <div className="flex flex-col items-center mx-12">
                         <li className="font-bold text-xl m-6">Seguimiento continuo</li>
                         <p className="text-xl text-center">Asignamos un equipo de trabajo específico proporcionando un servicio integral y personalizado durante todo el proceso.</p>
+                        <br />
                     </div>
+                </ul>
+                <ul className="grid grid-cols-4 gap-64 m-28  justify-items-center">
+                    <li className="flex  items-center justify-center text-4xl text-white h-20 w-20 font-bold  bg-linkIt-200 rounded-full">1</li>
+                    <li className="flex  items-center justify-center text-4xl text-white h-20 w-20 font-bold  bg-linkIt-200 rounded-full">2</li>
+                    <li className="flex  items-center justify-center text-4xl text-white h-20 w-20 font-bold  bg-linkIt-200 rounded-full">3</li>
+                    <li className="flex  items-center justify-center text-4xl text-white h-20 w-20 font-bold  bg-linkIt-200 rounded-full">4</li>
                 </ul>
             </div>
         </div>
