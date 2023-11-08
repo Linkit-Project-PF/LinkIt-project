62d5b1c5d232bc1182e8b8b4564d06a4f2c86040
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-08 16:02:25 -0500

    Placing div Module D Home

--- linkit-project-client/src/components/Home/Modulos/ModuloD/ModuloD.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloD/ModuloD.tsx
@@ -7,26 +7,26 @@ export default function ModuloD() {
                 <img className="z-0 " src="/public/vectores/linkit-web-vectores-06.svg" alt="360" />
                 <img className="image w-3/12 z-1 " src="/public/vectores/linkit-web-vectores-07.svg" alt="persons" />
             </div>
-            <div>
+            <div className='my-40'>
                 <ul>
-                    <div>
+                    <div className='flex flex-row'>
                         <img className='w-1/12' src="/public/vectores/linkit-web-vectores-08.svg" alt="" />
-                        <li>Proceso ágil</li>
-                        <p>En 5 días hábiles presentaremos talentos previamente entrevistados y calificados.</p>
-                        <div className='division'></div>
+                        <li className='font-bold text-2xl'>Proceso ágil</li>
                     </div>
-                    <div>
+                    <p>En 5 días hábiles presentaremos talentos previamente entrevistados y calificados.</p>
+                    <div className='division'></div>
+                    <div className='flex flex-row'>
                         <img className='w-1/12' src="/public/vectores/linkit-web-vectores-09.svg" alt="" />
-                        <li>Comunicación asertiva</li>
-                        <p>Nos comprometemos a mantener una comunicación ágil y efectiva durante todo el proceso de selección.</p>
-                        <div className='division'></div>
+                        <li className='font-bold text-2xl'>Comunicación asertiva</li>
                     </div>
-                    <div>
+                    <p>Nos comprometemos a mantener una comunicación ágil y efectiva durante todo el proceso de selección.</p>
+                    <div className='division'></div>
+                    <div className='flex flex-row'>
                         <img className='w-1/12' src="/public/vectores/linkit-web-vectores-10.svg" alt="" />
-                        <li>Consultoría 360°</li>
-                        <p>Te asesoramos desde el perfil ideal, los presupuestos idóneos y procesos de contratación hasta planes de beneficios, retención y mucho más.</p>
-                        <div className='division'></div>
+                        <li className='font-bold text-2xl'>Consultoría 360°</li>
                     </div>
+                    <p>Te asesoramos desde el perfil ideal, los presupuestos idóneos y procesos de contratación hasta planes de beneficios, retención y mucho más.</p>
+                    <div className='division'></div>
                 </ul>
             </div>
         </div>
