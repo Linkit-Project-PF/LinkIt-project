d0860702567b6f16d63e0e53bb77088a84eb5762
Jose Lozano <joselozano.dev@gmail.com>
2023-11-09 21:53:23 -0400

    Remove PhoneInput import from ModuloA component.

--- linkit-project-client/src/components/Home/Modulos/ModuloA/ModuloA.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloA/ModuloA.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom"
 import { motion } from "framer-motion"
-import PhoneInput from "react-phone-number-input/input"
 
 export default function ModuloA() {
 
