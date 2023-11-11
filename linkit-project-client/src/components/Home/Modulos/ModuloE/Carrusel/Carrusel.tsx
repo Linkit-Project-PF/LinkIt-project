759fb7073dd54fb6a1e1eb3a6bf06e8d3f2125cf
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-11 13:29:45 -0500

    Pre Carousel 2

--- linkit-project-client/src/components/Home/Modulos/ModuloE/Carrusel/Carrusel.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloE/Carrusel/Carrusel.tsx
@@ -9,38 +9,41 @@ export default function Carrusel() {
     }
 
     return (
-                <motion.div
-                    className="pl-[60px] whitespace-nowrap "
-                    initial="hidden"
-                    animate="visible"
-                    variants={slideVariants}
-                    transition={{
-                        type: "tween",
-                        duration: 15,
-                        ease: "linear",
-                        repeat: Infinity,
-                        
-                    }}
-                >
-                    {companies.map((c) => {
-                        return (
-                            <motion.img
-                                key={c.id}
-                                className="m-[40px] h-[50px] inline-block"
-                                src={c.logo}
-                            />
-                        )
-                    })}
-                    {companies.map((c) => {
-                        return (
-                            <motion.img
-                                key={c.id}
-                                className="m-[40px] h-[50px] inline-block"
-                                src={c.logo}
-                            />
-                        )
-                    })}
-                </motion.div >
+        <motion.div
+            className="-mx-[352px]  whitespace-nowrap w-auto flex gap-12"
+            initial="hidden"
+            animate="visible"
+            variants={slideVariants}
+            transition={{
+                type: "tween",
+                duration: 25,
+                ease: "linear",
+                repeat: Infinity,
+                /* repeatType: "mirror" */
+            }
+            }
+
+        >
+
+            {companies.map((c) => {
+                return (
+                    <motion.img
+                        key={c.id}
+                        className=" m-0 p-0 h-[50px] inline-block"
+                        src={c.logo}
+                    />
+                )
+            })}
+            {companies.map((c) => {
+                return (
+                    <motion.img
+                        key={c.id}
+                        className=" m-0 p-0 h-[50px] inline-block"
+                        src={c.logo}
+                    />
+                )
+            })}
+        </motion.div >
     )
 }
 
