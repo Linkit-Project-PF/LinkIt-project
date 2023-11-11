f3151fa18c065f94b8355a739d317ef15edbd25d
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-11 10:53:19 -0500

    Fixing bugs Carousel and Home

--- linkit-project-client/src/components/Home/Modulos/ModuloE/Carrusel/Carrusel.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloE/Carrusel/Carrusel.tsx
@@ -1,56 +1,46 @@
 import companies from "../../../../../Utils/companies.json"
-import { useState } from "react";
 import { motion } from 'framer-motion'
 
 export default function Carrusel() {
 
-   const items: number = 5;
-    const [currentPage, setCurrentPage] = useState(0);
-    const [animation, setAnimation] = useState('right');
-
-    const handleNext = () => {
-        setCurrentPage(currentPage + 1)
-        setAnimation('right')
+    const slideVariants = {
+        hidden: { x: 0 },
+        visible: { x: "-100%" },
     }
 
-    const handlePrevius = () => {
-        setCurrentPage(currentPage - 1)
-        setAnimation('left')
-    } 
-
-    const startIndex = currentPage * items
-    const endIndex = startIndex + items
-
-    const companiesFiltered = companies.slice(startIndex, endIndex)
-
     return (
-        <div className="flex flex-row justify-between px-20 py-2">
-            <button onClick={handlePrevius} disabled={currentPage === 0}>
-                <img src="Vectores/previus.png" alt="previus" />
-            </button>
-            {companiesFiltered.map((c) => {
-
-                const animateProps = animation === 'right' ? {opacity: 1, x:0, } : {opacity: 1, x:0, }
-                const initialProps = animation === 'right' ? {opacity: 0, x:100} : {opacity: 0, x:-100} 
-                const exitProps = animation === 'right' ? {opacity: 0, x:100} : {opacity: 0, x:-100} 
-                
-                return (
-                    <motion.div
-                        key={c.id}
-                        className="flex justify-center items-center overflow:hidden w-48 h-20"
-                        initial={initialProps}
-                        animate={animateProps}
-                        exit={exitProps}   
-                        transition={{ duration: 0.4 }}
-                    >
-                        <motion.img
-                            className="h-full w-full object-contain" src={c.logo} />
-                    </motion.div>
-                )
-            })}
-            <button onClick={handleNext} disabled={endIndex > companies.length}>
-                <img src="Vectores/next.png" alt="next" />
-            </button>
-        </div>
+                <motion.div
+                    className="pl-[60px] whitespace-nowrap "
+                    initial="hidden"
+                    animate="visible"
+                    variants={slideVariants}
+                    transition={{
+                        type: "tween",
+                        duration: 15,
+                        ease: "linear",
+                        repeat: Infinity,
+                        
+                    }}
+                >
+                    {companies.map((c) => {
+                        return (
+                            <motion.img
+                                key={c.id}
+                                className="m-[40px] h-[50px] inline-block"
+                                src={c.logo}
+                            />
+                        )
+                    })}
+                    {companies.map((c) => {
+                        return (
+                            <motion.img
+                                key={c.id}
+                                className="m-[40px] h-[50px] inline-block"
+                                src={c.logo}
+                            />
+                        )
+                    })}
+                </motion.div >
     )
 }
+
--- linkit-project-client/src/components/Home/Modulos/ModuloE/ModuloE.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloE/ModuloE.tsx
@@ -1,5 +1,4 @@
 import Carrusel from "./Carrusel/Carrusel";
-import Carrusel2 from "./Carrusel/Carrusel2";
 
 
 export default function ModuloE() {
@@ -7,7 +6,7 @@ export default function ModuloE() {
     <div className="pb-32">
         <h1 className="flex justify-center text-5xl font-bold my-20">Más de 500 empresas confían en LinkIT</h1>
         <Carrusel />
-        <Carrusel2 />
+
     </div>
   )
 }
