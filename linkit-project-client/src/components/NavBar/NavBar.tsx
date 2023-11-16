fa928ee561724bc2f452d9229fab269ad0e4a81b
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-16 18:11:42 -0500

    Conditional profile rendering

--- linkit-project-client/src/components/NavBar/NavBar.tsx
+++ linkit-project-client/src/components/NavBar/NavBar.tsx
@@ -263,7 +263,7 @@ function NavBar() {
                   <li className="relative top-3 mb-2 mt-3 ml-4 text-[10px] xl:text-xs">
                     <button
                       className="profile"
-                      onClick={console.log('Hola Mundo!')}
+                      onClick={}
                     >
                       Mi Perfil
                     </button>
