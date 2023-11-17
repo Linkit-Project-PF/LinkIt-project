faec3e5653f767f561023098ca0d7a5d3f8db4b7
Jose Lozano <joselozano.dev@gmail.com>
2023-11-17 16:46:33 -0400

    Update CSS styles and class names for blogs and ebooks cards

--- linkit-project-client/src/components/recursos/Modulos-Recursos/blogs/blogs-cards/BlogsCard.tsx
+++ linkit-project-client/src/components/recursos/Modulos-Recursos/blogs/blogs-cards/BlogsCard.tsx
@@ -44,7 +44,7 @@ function BlogsCard({ image, title, description, link, genre }: BlogsCardProps) {
 
   return (
     <motion.div 
-    className="flex flex-col border-[0.13rem] w-screen lg:w-[17rem] xl:w-[21rem] rounded-[0.625rem] font-montserrat text-[1.5rem] h-screen xl:h-[33rem] lg:h-[89vh] items-center justify-center bg-white card-container"
+    className="flex flex-col border-[0.13rem] w-screen lg:w-[17rem] xl:w-[21rem] rounded-[0.625rem] font-montserrat text-[1.5rem] h-screen xl:h-[33rem] lg:h-[89vh] items-center justify-center bg-white card-container container"
     key={key}
     variants={blogsCardVariants}
     initial="hidden"
@@ -56,24 +56,24 @@ function BlogsCard({ image, title, description, link, genre }: BlogsCardProps) {
         alt={title}
         className={`w-full h-[12rem] rounded-[0.625rem] ${
           title.length > 20 ? "xl:-mt-4" : ""
-        }`}
+        } image`}
       />
-      <div className="p-[3rem] flex flex-col flex-grow justify-between lg:w-full lg:p-[1rem] xl:p-[1.8rem]">
+      <div className="p-[3rem] flex flex-col flex-grow justify-between lg:w-full lg:p-[1rem] xl:p-[1.8rem] content-container">
         <div>
-          <p className="border-[2px] text-[0.8rem] mb-[10px] xl:mb-[1.5rem] h-[25px] border-linkIt-300 rounded-[10px] p-[0.8rem] font-semibold items-center justify-center whitespace-nowrap inline-flex lg:text-[0.7rem]">
+          <p className="border-[2px] text-[0.8rem] mb-[10px] xl:mb-[1.5rem] h-[25px] border-linkIt-300 rounded-[10px] p-[0.8rem] font-semibold items-center justify-center whitespace-nowrap inline-flex lg:text-[0.7rem] genre">
             {genre}
           </p>
-          <h1 className="font-bold w-full text-[1.3rem] lg:text-[1rem] xl:w-[17rem]">
+          <h1 className="font-bold w-full text-[1.3rem] lg:text-[1rem] xl:w-[17rem] title">
             {title}
           </h1>
-          <p className="font-semibold text-[0.9rem] mt-[15px] xl:mb-[1rem] xl:w-[17rem]">
+          <p className="font-semibold text-[0.9rem] mt-[15px] xl:mb-[1rem] xl:w-[17rem] description">
             {description}
           </p>
         </div>
         <motion.a
           href={link}
           target="_blank"
-          className="text-[1rem] font-bold "
+          className="text-[1rem] font-bold link"
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 1 }}
         >
--- linkit-project-client/src/components/recursos/Modulos-Recursos/ebooks/ebooksCards/EbooksCard.tsx
+++ linkit-project-client/src/components/recursos/Modulos-Recursos/ebooks/ebooksCards/EbooksCard.tsx
@@ -48,18 +48,18 @@ function EbooksCard({
   return (
     <motion.div
       key={key}
-      className="bg-white w-[60%] h-[50vh] xl:h-[17rem] font-montserrat rounded-[0.75rem] flex flex-col justify-between p-[2rem] lg:p-[1.5rem] lg:h-[55vh]"
+      className="bg-white w-[60%] h-[50vh] xl:h-[17rem] font-montserrat rounded-[0.75rem] flex flex-col justify-between p-[2rem] lg:p-[1.5rem] lg:h-[55vh] container-ebook"
       variants={cardVariants}
       initial={"initial"}
       animate={"animate"}
       exit={"exit"}
     >
       <div>
-        <p className="border-[2px] text-[0.8rem] mb-[1rem] h-[25px] lg:h-[10px] border-linkIt-300 rounded-[10px] p-[0.8rem] lg:p-[0.7rem] font-semibold items-center justify-center whitespace-nowrap inline-flex lg:text-[0.7rem]">
+        <p className="border-[2px] text-[0.8rem] mb-[1rem] h-[25px] lg:h-[10px] border-linkIt-300 rounded-[10px] p-[0.8rem] lg:p-[0.7rem] font-semibold items-center justify-center whitespace-nowrap inline-flex lg:text-[0.7rem] category">
           {category}
         </p>
-        <h1 className="font-bold text-[1.3rem] w-[100%] mb-[0.9rem] lg:text-[1rem] lg:mt-[0.5rem]">{title}</h1>
-        <p className="font-semibold text-[0.8rem] mb-[1.5rem] xl:mb-[-1rem]">{description}</p>
+        <h1 className="font-bold text-[1.3rem] w-[100%] mb-[0.9rem] lg:text-[1rem] lg:mt-[0.5rem] title">{title}</h1>
+        <p className="font-semibold text-[0.8rem] mb-[1.5rem] xl:mb-[-1rem] description">{description}</p>
       </div>
       <motion.a 
       href={link} 
