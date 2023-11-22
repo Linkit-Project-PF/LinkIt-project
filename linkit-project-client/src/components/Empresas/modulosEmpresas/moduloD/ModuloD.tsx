6460b5a72d7ae69df63223e30842db61c2210e2e
jimenzedwar <jimenzeddi@gmail.com>
2023-11-22 13:26:30 -0500

    Fix styling issues in ModuloD and ModuloE components

--- linkit-project-client/src/components/Empresas/modulosEmpresas/moduloD/ModuloD.tsx
+++ linkit-project-client/src/components/Empresas/modulosEmpresas/moduloD/ModuloD.tsx
@@ -11,7 +11,7 @@ export default function ModuloA() {
     <div className="flex flex-col items-center bg-linkIt-500 py-10 xl:py-16 2xl:py-20">
       <h1 className="flex font-bold text-[2vw]">Lo que dicen nuestros clientes</h1>
       <motion.button className="flex justify-center items-center p-2 text-[1vw] mt-[2vh] xl:text-[0.7vw] border-linkIt-300 border rounded-lg font-medium shadow-md hover:bg-linkIt-300 hover:shadow-md hover:text-white transition-all duration-300 ease-in-out" whileTap={{ scale: 0.9 }}>Conoce los casos de éxito</motion.button>
-      <div className="grid grid-cols-4 space-x-3 mt-16 mb-24">
+      <div className="grid grid-cols-4 space-x-3 mt-16 mb-[10vh]">
       <div className="grid grid-col space-y-3">
         {
           testimonios.slice(0, 2).map((testimonio) => (
