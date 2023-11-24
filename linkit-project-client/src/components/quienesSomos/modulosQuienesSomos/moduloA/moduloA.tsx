214cd9de9606a91a0978dff14a4172d8c99301e6
jimenzedwar <jimenzeddi@gmail.com>
2023-11-24 00:32:54 -0500

    Add ModuloA component to QuienesSomos

---
+++ linkit-project-client/src/components/quienesSomos/modulosQuienesSomos/moduloA/moduloA.tsx
@@ -0,0 +1,12 @@
+export default function ModuloA() {
+    return (
+        <div className="bg-linkIt-200 h-screen grid grid-cols-2 p-[5vw]">
+            <div>
+            <h1 className="text-white text-[2.6vw] mt-[13vh] font-manrope font-bold tracking-wide">En LinkIT buscamos conectar al talento más destacado con los mejores proyectos de tecnología de manera global.</h1>
+            <p className="col-start-1 text-white text-[1.6vw] font-montserrat mt-[3vh]">Tenemos el compromiso de generar un mundo más inclusivo, democratizando oportunidades, y facilitando la adquisición y gestión de los equipos.</p>
+            </div>
+        <img src="/Vectores/linkit-logos-web_2-logo-ppal-blanco.svg" alt="logo-ppal-blanco" className="ml-[7vw] w-5/6 mt-[2vh]" />
+        <hr className="col-span-2" />
+        </div>
+    )
+}
--- linkit-project-client/src/components/quienesSomos/quienesSomos.tsx
+++ linkit-project-client/src/components/quienesSomos/quienesSomos.tsx
@@ -1,8 +1,10 @@
+import ModuloA from "./modulosQuienesSomos/moduloA/moduloA";
 
 
 export default function QuienesSomos() {
     return (
         <div>
+            <ModuloA />
             <img src="/Vectores/construc.jpg" alt="" />
         </div>
     )
