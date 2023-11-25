bc7b991571b8c3628a0d1281df3b5d78002e1718
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-25 11:20:12 -0500

    Fixing logout bugs

--- linkit-project-client/src/components/NavBar/NavBar.tsx
+++ linkit-project-client/src/components/NavBar/NavBar.tsx
@@ -337,7 +337,7 @@ function NavBar() {
                   <li className="relative top-3 mb-2 mt-3 ml-4 text-[10px] xl:text-xs">
                     <button
                       className="logout"
-                      onClick={() => dispatch(logout())}
+                      onClick={handleLogout}
                     >
                       Cerrar sesión
                     </button>
@@ -385,7 +385,7 @@ function NavBar() {
                   <li className="relative top-3 mb-2 mt-3 ml-4 text-[10px] xl:text-xs">
                     <button
                       className="logout"
-                      onClick={() => dispatch(logout())}
+                      onClick={handleLogout}
                     >
                       Cerrar sesión
                     </button>
