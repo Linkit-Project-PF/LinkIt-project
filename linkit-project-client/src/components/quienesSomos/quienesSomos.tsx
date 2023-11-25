30fb409e8cd946621c2e2f4147bbebe31075d2cd
jimenzedwar <jimenzeddi@gmail.com>
2023-11-24 22:42:22 -0500

    Fix styling issues in ModuloI and ModuloA components

--- linkit-project-client/src/components/quienesSomos/quienesSomos.tsx
+++ linkit-project-client/src/components/quienesSomos/quienesSomos.tsx
@@ -1,10 +1,12 @@
 import ModuloA from "./modulosQuienesSomos/moduloA/moduloA";
+import ModuloB from "./modulosQuienesSomos/moduloB/moduloB";
 
 
 export default function QuienesSomos() {
     return (
         <div>
             <ModuloA />
+            <ModuloB />
             <img src="/Vectores/construc.jpg" alt="" />
         </div>
     )
