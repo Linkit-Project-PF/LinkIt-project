61784c972d472e51cb1122ad7989d97a2322c9e0
jimenzedwar <jimenzeddi@gmail.com>
2023-11-27 17:57:06 -0500

    Refactor module G and F, update styles and add images

--- linkit-project-client/src/components/quienesSomos/modulosQuienesSomos/moduloF/moduloF.tsx
+++ linkit-project-client/src/components/quienesSomos/modulosQuienesSomos/moduloF/moduloF.tsx
@@ -1,9 +1,16 @@
  export default function ModuloF() { 
         return (
-            <div className="bg-linkIt-200">
-                <h2>
-                    En proceso
-                </h2>
+            <div className="relative bg-linkIt-200 z-10 px-[6vw] grid grid-cols-4 before:absolute before:bg-linkIt-500 before:w-[90vh] xl:before:w-[75vh] before:top-[35vh] xl:before:top-[26vh] 2xl:before:w-[60vh] before:h-[75vh] xl:before:h-[65vh] 2xl:before:h-[60vh] before:-z-[1] before:skew-y-[40deg] before:left-0 2xl:before:top-[20vh] gap-x-4">
+                <img src="/people-LinkIt/ary-perfil.png" alt="ary-perfil" className="relative bottom-0 col-start-1 row-start-1" />
+                <div className="grid grid-cols-2 mb-3">
+                <img src="/Vectores/comillas-blancas.svg" alt="Comillas" className="w-1/4 object-contain mt-[8vh] 2xl:mt-[10vh] col-start-1" />
+                <h1 className="text-white font-montserrat w-[40vw] text-[1vw] mt-[10vh] 2xl:mt-[12vh] -ml-[7vw] text-start grid-cols-2">
+                LinkIT nace para encontrar una solución clara, conectar al talento mas destacado con los mejores proyectos IT. Actuamos como enlace para simplificar la experiencia en el proceso de contratación, con el objetivo de formar equipos de alto rendimiento.
+                </h1>
+                <p className="text-white text-[1vw] col-start-2 text-start font-montserrat font-bold -ml-[7vw] 2xl:-mt-10">Ary Molchadsky <br />
+                <span className="text-white col-start-2 text-start font-manrope font-normal">CEO & Co-Founder</span></p>
+                
+                </div>
             </div>
         )
  }
--- linkit-project-client/src/components/quienesSomos/modulosQuienesSomos/moduloG/moduloG.tsx
+++ linkit-project-client/src/components/quienesSomos/modulosQuienesSomos/moduloG/moduloG.tsx
@@ -1,8 +1,8 @@
 export default function ModuloG() {
     return (
-        <div className="h-screen grid grid-col p-[6vw]">
+        <div className="relative h-screen grid grid-col p-[6vw] z-10 bg-white before:absolute before:bg-linkIt-200 before:w-screen before:h-1 before:z-10 before:-skew-y-1 before:-top-2 after:block after:bg-white after:w-screen after:h-[6vh] after:absolute after:-skew-y-1 after:-top-4">
             <h1 className="text-black text-[2.6vw] font-manrope font-bold tracking-wide text-center self-center justify-center">Conoce a alguno de los integrantes de nuestro equipo</h1>
-            <div className="grid grid-cols-4" >
+            <div className="grid grid-cols-4 z-30 bg-white" >
                     <img src="/people-LinkIt/philo-perfil.png" className="bg-linkIt-500 rounded-lg" alt="philo-perfil" />
                     <img src="/people-LinkIt/ary-perfil.png" className=" bg-linkIt-500 rounded-lg" alt="ary-perfil" />
                     <img src="/people-LinkIt/gonza-perfil.png" className=" bg-linkIt-500 rounded-lg" alt="gonza-perfil" />
