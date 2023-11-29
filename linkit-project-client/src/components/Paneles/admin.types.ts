9a93fa677ee12a6b33085d84e251b06254f35859
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-11-29 11:00:43 -0500

    Redux for reviews

--- linkit-project-client/src/components/Paneles/admin.types.ts
+++ linkit-project-client/src/components/Paneles/admin.types.ts
@@ -23,12 +23,19 @@ export type vacancyProps = {
 
 export type ResourceProps = {
   _id: string;
-  title: string, //
+  title: string, 
   description: string,
-  link: string, //
-  type: string, //
+  link: string, 
+  type: string, 
   createdDate: string,
-  image: string, //
-  category: string, //
+  image: string, 
+  category: string, 
   archived: boolean,
+}
+
+export type Review = {
+  nameUserOrCompany: string,
+  rol: string
+  country: string
+  detail: string
 }
