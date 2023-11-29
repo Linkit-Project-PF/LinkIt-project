c022b29720079ac1aaf0cafc61d87415a5c5037f
Maria Gabriela Sándigo Muñoz <137342274+MGSandigoMunoz@users.noreply.github.com>
2023-11-29 12:26:44 -0500

    Admin review EN translation

--- linkit-project-client/src/components/Paneles/AdminProfile/Panel/AdminReviews/AdminReviews.tsx
+++ linkit-project-client/src/components/Paneles/AdminProfile/Panel/AdminReviews/AdminReviews.tsx
@@ -1,9 +1,10 @@
-
+import { useTranslation } from "react-i18next"
 
 export default function AdminReviews() {
+  const {t}=useTranslation()
   return (
     <div>
-        <h1 className="text-5xl pl-32 pb-4">Gestión de reviews</h1>
+        <h1 className="text-5xl pl-32 pb-4">{t('Gestión de reseñas')}</h1>
     </div>
   )
 }
