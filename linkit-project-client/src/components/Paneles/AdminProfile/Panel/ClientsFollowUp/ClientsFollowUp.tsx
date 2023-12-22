7a6cc61f1a32dad96d90c9cc0bd91cee4da818f6
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-12-22 06:44:49 -0500

    All view of Clients Follow UP ok

--- linkit-project-client/src/components/Paneles/AdminProfile/Panel/ClientsFollowUp/ClientsFollowUp.tsx
+++ linkit-project-client/src/components/Paneles/AdminProfile/Panel/ClientsFollowUp/ClientsFollowUp.tsx
@@ -116,6 +116,20 @@ export default function ClientsFollowUp() {
     setCurrentPage(currentPage - 1);
   };
 
+  const renderSectionBasic = <K extends keyof ClientFollowUpProps>(title: string, key: K,) => (
+    <div>
+      <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
+        <h1>{title}</h1>
+      </div>
+      <div>
+        {dataToShow.map((r: ClientFollowUpProps) => (
+          <p key={String(r[key])} className='pl-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50'>{String(r[key])}</p>
+        ))}
+      </div>
+    </div>
+
+  )
+
   return (
     <div>
       <div className=' bg-scroll bg-linkIt-500'>
@@ -130,9 +144,9 @@ export default function ClientsFollowUp() {
 
           {viewCol["Recruitment role code"] &&
             <div className=''>
-              <div className='flex flex-row px-16 border-b-2 border-r-2  border-linkIt-200'>
+              <div className='flex flex-row whitespace-nowrap px-16 border-b-2 border-r-2  border-linkIt-200'>
                 <div className='justify-start'>
-                  <h1>Recruitment role code</h1>
+                  <h1>Recruitment Role Code</h1>
                 </div>
               </div>
               <div className=''>
@@ -145,6 +159,56 @@ export default function ClientsFollowUp() {
               </div>
             </div>
           }
+
+          {viewCol.Client && renderSectionBasic("Client", "Client")}
+          {viewCol["Role Name"] && renderSectionBasic("Role Name", "Role Name")}
+          {viewCol["Tipo de cliente"] && renderSectionBasic("Tipo de cliente", "Tipo de cliente")}
+          {viewCol["Lider de la búsqueda"] && renderSectionBasic("Lider de la búsqueda", "Lider de la búsqueda")}
+          {viewCol.Status && renderSectionBasic("Status", "Status")}
+          {viewCol["Tipo de oportunidad"] && renderSectionBasic("Tipo de oportunidad", "Tipo de oportunidad")}
+          {viewCol["Role Code"] && renderSectionBasic("Role Code", "Role Code")}
+          {viewCol.JD && renderSectionBasic("JD", "JD")}
+          {viewCol["Talent Pool Stack"] && renderSectionBasic("Talent Pool Stack", "Talent Pool Stack")}
+          {viewCol["Años de experiencia mínimo"] && renderSectionBasic("Años de experiencia mínimo", "Años de experiencia mínimo")}
+          {viewCol.Seniority && renderSectionBasic("Seniority", "Seniority")}
+          {viewCol["Cantidad de vacantes"] && renderSectionBasic("Cantidad de vacantes", "Cantidad de vacantes")}
+          {viewCol.Responsable && renderSectionBasic("Responsable", "Responsable")}
+          {viewCol["Contact Name"] && renderSectionBasic("Contact Name", "Contact Name")}
+          {viewCol.BUDGET && renderSectionBasic("BUDGET", "BUDGET")}
+          {viewCol["BUDGET in USD"] && renderSectionBasic("BUDGET in USD", "BUDGET in USD")}
+          {viewCol.Country && renderSectionBasic("Country", "Country")}
+          {viewCol["English Level"] && renderSectionBasic("English Level", "English Level")}
+          {viewCol["Modalidad de empleo"] && renderSectionBasic("Modalidad de empleo", "Modalidad de empleo")}
+          {viewCol["On-site / Remote"] && renderSectionBasic("On-site / Remote", "On-site / Remote")}
+          {viewCol["Hourly Type"] && renderSectionBasic("Hourly Type", "Hourly Type")}
+          {viewCol["Fee acordado"] && renderSectionBasic("Fee acordado", "Fee acordado")}
+          {viewCol["Total candidates endorsed"] && renderSectionBasic("Total candidates endorsed", "Total candidates endorsed")}
+          {viewCol["Alignment/Start date"] && renderSectionBasic("Alignment/Start date", "Alignment/Start date")}
+          {viewCol["1st endorsement"] && renderSectionBasic("1st endorsement", "1st endorsement")}
+          {viewCol["1st Client interview"] && renderSectionBasic("1st Client interview", "1st Client interview")}
+          {viewCol["2nd Client interview"] && renderSectionBasic("2nd Client interview", "2nd Client interview")}
+          {viewCol["1st Offer"] && renderSectionBasic("1st Offer", "1st Offer")}
+          {viewCol["Talent Start Date"] && renderSectionBasic("Talent Start Date", "Talent Start Date")}
+          {viewCol["Role Closed date"] && renderSectionBasic("Role Closed date", "Role Closed date")}
+          {viewCol.Area && renderSectionBasic("Area", "Area")}
+          {viewCol["Reasons closed lost"] && renderSectionBasic("Reasons closed lost", "Reasons closed lost")}
+          {viewCol["Nombre talento"] && renderSectionBasic("Nombre talento", "Nombre talento")}
+          {viewCol["Closed Rate"] && renderSectionBasic("Closed Rate", "Closed Rate")}
+          {viewCol.Comments && renderSectionBasic("Comments", "Comments")}
+          {viewCol["Last Modified"] && renderSectionBasic("Last Modified", "Last Modified")}
+          {viewCol["Prospect type"] && renderSectionBasic("Prospect type", "Prospect type")}
+          {viewCol["Created By"] && renderSectionBasic("Created By", "Created By")}
+          {viewCol.created && renderSectionBasic("Created", "created")}
+          {viewCol["Created time Month"] && renderSectionBasic("Created time Month", "Created time Month")}
+          {viewCol["Endorsement time"] && renderSectionBasic("Endorsement time", "Endorsement time")}
+          {viewCol["Cl. Interview time"] && renderSectionBasic("Cl. Interview time", "Cl. Interview time")}
+          {viewCol["Time to offer"] && renderSectionBasic("Time to offer", "Time to offer")}
+          {viewCol["Time to fill"] && renderSectionBasic("Time to fill", "Time to fill")}
+          {viewCol["Mes fecha cierre"] && renderSectionBasic("Mes fecha cierre", "Mes fecha cierre")}
+          {viewCol["Año fecha cierre"] && renderSectionBasic("Año fecha cierre", "Año fecha cierre")}
+          {viewCol["Sales code"] && renderSectionBasic("Sales code", "Sales code")}
+          {viewCol["To today"] && renderSectionBasic("To today", "To today")}
+
 
         </div>
         <div className="flex flex-row justify-around">
