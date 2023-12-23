0a0694446c26ad6cde82e7893268177718b2564b
MateoGuerreroE <mateof1223@gmail.com>
2023-12-23 10:46:17 -0500

    Fixed user list and left some notes on admin profile

--- linkit-project-client/src/redux/types.ts
+++ linkit-project-client/src/redux/types.ts
@@ -1,18 +1,18 @@
-import { IUser, ICompany } from "../components/Profiles/types"
+import { IUser, ICompany } from "../components/Profiles/types";
 
 export interface IAuthState {
-  isAuthenticated: boolean
-  role: string
-  user: IUser | null
-  company: ICompany | null
-  token: string | null
+  isAuthenticated: boolean;
+  role: string;
+  user: IUser | null;
+  company: ICompany | null;
+  token: string | null;
 }
 
 export interface RootState {
-  Authentication: IAuthState
-  registerLogin: any
-  jobCard: any
-  resources: any
+  Authentication: IAuthState;
+  registerLogin: any;
+  jobCard: any;
+  resources: any;
 }
 
 export interface JobData {
