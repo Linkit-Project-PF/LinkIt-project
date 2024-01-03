594e961494b8eb6381e5d0f90342c5ecd985855d
jimenzedwar <jimenzeddi@gmail.com>
2024-01-03 00:32:30 -0400

    home mobile 90%

--- linkit-project-client/src/components/Home/Modulos/ModuloF/ModuloF.tsx
+++ linkit-project-client/src/components/Home/Modulos/ModuloF/ModuloF.tsx
@@ -15,15 +15,15 @@ export default function ModuloF() {
     }
 
     return (
-        <div className="relative bg-linkIt-500 dark:bg-linkIt-200 dark:text-white">
-            <div className="my-[10%] ml-[6%] mb-[15%]">
-                    <h1 className="relative z-10 font-extrabold text-[4vw] w-[37%] justify-self-start leading-tight font-manrope">{t('Talento y empresas en más de 50 países')}</h1>
+        <div className="relative bg-linkIt-500 dark:bg-linkIt-200 dark:text-white overflow-hidden p-[7%] w-screen">
+            <div className="">
+                    <h1 className="relative z-10 font-extrabold text-[4vw] w-[40%] justify-self-start leading-tight font-manrope">{t('Talento y empresas en más de 50 países')}</h1>
                     <div className="relative z-10 mt-[3%] space-x-2">
                         <motion.button className="background-button" onClick={goSoyEmpresa} whileTap={{ scale: 0.9 }}>{t('Contrata Talento')}</motion.button>
                         <motion.button className="transparent-background-button" onClick={goSoyTalento} whileTap={{ scale: 0.9 }}>{t('Vacantes disponibles')}</motion.button>
                     </div>
                     </div>
-                        <img className="absolute z-10 top-[-10.5%] w-[70%] left-[3%]" src="/Vectores/linkit-web-vectores-11.svg" alt="world map" />
+                        <img className="absolute top-[-13%] md:top-[-14%] left-[50%] w-[70%] lg:left-[35%] z-0" src="/Vectores/linkit-web-vectores-11.svg" alt="world map" />
         </div>
     )
 }
