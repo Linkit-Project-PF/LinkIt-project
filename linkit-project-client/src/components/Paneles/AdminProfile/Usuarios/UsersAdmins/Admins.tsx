5176ece07651947cac0508c8b9bc8cfbff4df354
Samuel Rodríguez <samuelmateo986@gmail.com>
2024-01-04 06:07:28 -0500

    Select Admin users edit

--- linkit-project-client/src/components/Paneles/AdminProfile/Usuarios/UsersAdmins/Admins.tsx
+++ linkit-project-client/src/components/Paneles/AdminProfile/Usuarios/UsersAdmins/Admins.tsx
@@ -7,7 +7,7 @@ import { Admin } from "../../../admin.types";
 
 type stateProps = {
   users: {
-      admins: Admin[];
+    admins: Admin[];
   };
 };
 
@@ -31,21 +31,25 @@ export default function Admins() {
 
 
   useEffect(() => {
-      const loadData = async (): Promise<void> => {
-          try {
-              const response = await axios(
-                  "https://linkit-server.onrender.com/admins/find",
-                  { headers: { Authorization: `Bearer ${token}`,
-                  'Accept-Language': sessionStorage.getItem('lang') } }
-              );
-              dispatch(setUsersAdmins(response.data));
-          } catch (error) {
-              console.error("Error al cargar las información", error);
+    const loadData = async (): Promise<void> => {
+      try {
+        const response = await axios(
+          "https://linkit-server.onrender.com/admins/find",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Accept-Language': sessionStorage.getItem('lang')
+            }
           }
-      };
-      loadData();
+        );
+        dispatch(setUsersAdmins(response.data));
+      } catch (error) {
+        console.error("Error al cargar las información", error);
+      }
+    };
+    loadData();
   }, [])
-  
+
 
   const itemsPerPage = 15;
   const [currentPage, setCurrentPage] = useState(0);
@@ -58,63 +62,108 @@ export default function Admins() {
   const totalPages = Math.ceil(data.length / itemsPerPage);
 
   const handleNext = (): void => {
-      setCurrentPage(currentPage + 1);
+    setCurrentPage(currentPage + 1);
   };
 
   const handlePrevius = (): void => {
-      setCurrentPage(currentPage - 1);
+    setCurrentPage(currentPage - 1);
   };
 
   const hideCol = (e: React.ChangeEvent<HTMLInputElement>): void => {
-      const { name } = e.target
-      setViewCol((prevViewCol) => ({
-          ...prevViewCol,
-          [name]: !prevViewCol[name as keyof typeof prevViewCol],
-      }))
+    const { name } = e.target
+    setViewCol((prevViewCol) => ({
+      ...prevViewCol,
+      [name]: !prevViewCol[name as keyof typeof prevViewCol],
+    }))
+  }
+
+  const [selectedRows, setSelectedRows] = useState<Set<string>>(new Set())
+  const [/*editing*/, setEditing] = useState(false)
+
+  const handleEdit = (id: string): void => {
+    const updateSelectedRows = new Set(selectedRows);
+    if (updateSelectedRows.has(id)) {
+      updateSelectedRows.delete(id)
+    } else {
+      updateSelectedRows.add(id)
+    }
+    setSelectedRows(updateSelectedRows)
+    setEditing(false)
   }
 
   const renderSectionBasic = <K extends keyof Admin>(title: string, key: K,) => (
 
-      <div>
-          <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
-              <h1>{title}</h1>
-          </div>
-          <div>
-              {dataToShow.map((r: Admin, index) => (
-                  <p key={`${key}-${index}`} className='pl-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50'>{String(r[key] === undefined || NaN ? '' : r[key])}</p>
-              ))}
-          </div>
+    <div>
+      <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
+        <h1>{title}</h1>
       </div>
+      <div>
+        {dataToShow.map((r: Admin, index) => (
+          <div
+          key={`${key}-${index}`}  
+          className={selectedRows.has(r._id) ? 'pl-3 pr-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 bg-linkIt-300 justify-center items-center' : 'pl-3 pr-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50 justify-center items-center'}
+          >
+          <p>{String(r[key] === undefined || NaN ? '' : r[key])}</p>
+          </div>
+        ))}
+      </div>
+    </div>
 
   )
+
+  const renderSectionSelect = <K extends keyof Admin>(title: string, key: K,) => (
+
+    <div>
+      <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
+        <h1>{title}</h1>
+      </div>
+      <div>
+        {dataToShow.map((r: Admin, index) => (
+          <div
+            key={`${key}-${index}`}
+            className={selectedRows.has(r._id) ? 'capitalize flex flex-row  pl-3 h-8 pt-1 bg-linkIt-300 whitespace-nowrap' : 'capitalize flex flex-row  pl-3 h-8 pt-1 border-b-2 border-r-2 border-linkIt-50 overflow-ellipsis overflow-hidden line-clamp-1'}
+          >
+            <input type="checkbox" name="edit" onChange={() => handleEdit(r._id)} checked={selectedRows.has(r._id)} />
+            <p className='pl-2'>{String(r[key] === undefined || NaN ? '' : r[key])}
+
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+
   const renderSectionBasicCap = <K extends keyof Admin>(title: string, key: K,) => (
 
-      <div>
-          <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
-              <h1>{title}</h1>
-          </div>
-          <div>
-              {dataToShow.map((r: Admin, index) => (
-                  <p key={`${key}-${index}`} className='capitalize pl-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50'>{String(r[key] === undefined || NaN ? '' : r[key])}</p>
-              ))}
-          </div>
+    <div>
+      <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
+        <h1>{title}</h1>
       </div>
-
+      <div>
+        {dataToShow.map((r: Admin, index) => (
+          <div 
+          key={`${key}-${index}`}
+          className={selectedRows.has(r._id) ? 'pl-3 pr-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 bg-linkIt-300 justify-center items-center' : 'pl-3 pr-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50 justify-center items-center'}
+          >
+          <p>{String(r[key] === undefined || NaN ? '' : r[key])}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 
   const renderSectionActive = <K extends keyof Admin>(title: string, key: K,) => (
 
-      <div>
-          <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
-              <h1>{title}</h1>
-          </div>
-          <div>
-              {dataToShow.map((r: Admin, index) => (
-                  <p key={`${key}-${index}`} className='capitalize pl-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50'>{String(r[key] === true ? 'Activo' : 'Inactivo')}</p>
-              ))}
-          </div>
+    <div>
+      <div className='flex flex-row whitespace-nowrap px-20 border-b-2 border-r-2  w-80 border-linkIt-200'>
+        <h1>{title}</h1>
       </div>
-
+      <div>
+        {dataToShow.map((r: Admin, index) => (
+          <p key={`${key}-${index}`} className='capitalize pl-3 pt-1 overflow-hidden overflow-ellipsis h-8 w-80 line-clamp-1 border-b-2 border-r-2 border-linkIt-50'>{String(r[key] === true ? 'Activo' : 'Inactivo')}</p>
+        ))}
+      </div>
+    </div>
   )
 
 
@@ -125,7 +174,7 @@ export default function Admins() {
         viewCol={viewCol}
       />
       <div className='flex flex-row mx-6 overflow-y-scroll border-2 border-linkIt-200 rounded-lg'>
-        {viewCol.rol && renderSectionBasicCap("Rol", "role")}
+        {viewCol.rol && renderSectionSelect("Rol", "role")}
         {viewCol.nombre && renderSectionBasicCap("Nombre", "firstName")}
         {viewCol.apellido && renderSectionBasicCap("Apellido", "lastName")}
         {viewCol.pais && renderSectionBasicCap("País", "country")}
