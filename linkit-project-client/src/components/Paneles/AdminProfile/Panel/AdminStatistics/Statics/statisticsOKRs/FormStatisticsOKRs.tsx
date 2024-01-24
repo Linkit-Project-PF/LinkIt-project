1f41f69a2f35e4343c4a8be0d1356a0527a3acaf
Maria Gabriela Sándigo Muñoz <137342274+MGSandigoMunoz@users.noreply.github.com>
2024-01-24 18:29:24 -0500

    OKR:create_ready_pending_styles

--- linkit-project-client/src/components/Paneles/AdminProfile/Panel/AdminStatistics/Statics/statisticsOKRs/FormStatisticsOKRs.tsx
+++ linkit-project-client/src/components/Paneles/AdminProfile/Panel/AdminStatistics/Statics/statisticsOKRs/FormStatisticsOKRs.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import swal from 'sweetalert';
 // import { SpecificOKRsArea, Area, OKRsType } from "../../../admin.types";
@@ -32,25 +32,42 @@ export type stateProps = {
     
     const [viewForm, setViewForm] = useState(false);
     
-    const [areaName, setAreaName] = useState("")
+    const [lastAreaName, setLastAreaName] = useState("")
+
     const [specificOKRsAreaComplete, setSpecificOKRsAreaComplete] = useState<SpecificOKRsArea>({
-          
-          okrSpecificName:"",
-          okrsSpecific:[]
-        }
-        );
+      
+      okrSpecificName:"",
+      okrsSpecific:[]
+    }
+    );
+   
+    const [arraySpecificOKRsArea, setArraySpecificOKRsArea] = useState([
+      specificOKRsAreaComplete
+    ])
+
+    const [arrayAreas, setArrayAreas] = useState([
+      {
+        name:lastAreaName,
+        specificOKRsArea:arraySpecificOKRsArea || []
+      }
+    ])
+
   const [specificResultName, setSpecificResultName] = useState<string>("");
+
+  // const [isFormSpecificResultsOpen, setIsFormSpecificResultsOpen] = useState(false);
+
+  
   const [completeOKR, setCompleteOKR] = useState<Partial<OKRsType>>({
     generalTitleOKR:"",
     areas:[
       {
         name:"",
-            specificOKRsArea:[
-              {
-                okrSpecificName:"",
-                okrsSpecific:[]
-                }
-            ]
+        specificOKRsArea:[
+          {
+            okrSpecificName:"",
+            okrsSpecific:[]
+            }
+        ]
           }
         ],
         archived:false
@@ -73,8 +90,30 @@ export type stateProps = {
     // ],
     // archived:false
     // });
-
     
+  
+      //!CAMBIO HECHO
+      //!CAMBIO HECHO
+      //!CAMBIO HECHO
+//! REVISAR PARA ELIMINAR LOS VACíOS EN EL ARRAY arraySpecificOKRsArea 
+        
+    useEffect(() => {
+  // Filtra los objetos que cumplen la condición okrSpecificName !== ""
+  const filteredArray = arraySpecificOKRsArea.filter(specificOKR => specificOKR.okrSpecificName !== "");
+
+  // Añade el nuevo objeto solo si okrSpecificName no es una cadena vacía
+  if (specificOKRsAreaComplete.okrSpecificName !== "") {
+    // Añade el nuevo objeto al array filtrado
+    filteredArray.push(specificOKRsAreaComplete);
+  }
+
+  // Actualiza el estado con el nuevo array filtrado
+  setArraySpecificOKRsArea(filteredArray);
+
+}, [specificOKRsAreaComplete]);
+
+     
+  console.log(arraySpecificOKRsArea)
     
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
       const { name, value } = e.target;
@@ -96,119 +135,145 @@ export type stateProps = {
       
   const handleAreaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
   const areaName = e.target.value;
-  const areaAlreadyExists = completeOKR.areas?.some((area)=> area.name=== areaName)
+
+  const areaAlreadyExists = arrayAreas?.some((area)=> area.name=== areaName)
   
   if (!areaAlreadyExists){
 
-    setAreaName(areaName);
+  setSpecificOKRsAreaComplete({
+    okrSpecificName:"",
+    okrsSpecific:[]
+  });
 
-      setShowForm(true)
+  setArraySpecificOKRsArea([
+    specificOKRsAreaComplete
+  ])
+
+    setLastAreaName(areaName);
+    setShowForm(true);
   } else {
-      setShowForm(false)
+    setShowForm(false)
     swal(t('Esta área ya existe'));
     console.error()
   }
 }
-const noShowForm = () => {  
-  setViewForm(false);
-};
 
 
 const handleResultNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
   setSpecificResultName(e.target.value);
 };
 
-
-const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // const validationError = validations(completeOKR as ResourceProps);
-    // setErrors(validationError);
-    
-    try {
-        const endPoint = "https://linkit-server.onrender.com/OKRs/create";
-        const response = await axios.post(endPoint, completeOKR, {
-          headers: { Authorization: `Bearer ${token}`,
-          'Accept-Language': sessionStorage.getItem('lang')}
-        });
-        
-        swal(t("El OKR fue creado con éxito"));
-        setCompleteOKR(
-          {
-            generalTitleOKR:"",
-            areas:[
-              {
-                    name:"",
-                    specificOKRsArea:[
-                        {
-                        okrSpecificName:"",
-                        okrsSpecific:[]
-                        }
-                    ]
-                }
-            ],
-            archived:false
-        });
-        onClose()
-        return response.data;
-      } catch (error: any) {
-        console.error(error.response.data)
-      }
-    };
+const noShowForm = () => {  
+  setViewForm(false);
   
-    // const [okrsSpecific, setOkrsSpecific] = useState<string[]>([]);
-
-    // const handleOkrsSpecificUpdate = (newOkrsSpecific: string[]) => {
-    //     setOkrsSpecific(newOkrsSpecific);
-    //   };
-
-    //   const showForm = () => {
-    //     setViewForm(true);
-    // };
-
-    // const noShowForm = () => {  
-    //     setViewForm(false);
-    // };
-
-    const lastAreaName = completeOKR.areas?.length
-    ? completeOKR.areas[completeOKR.areas.length - 1].name
-    : '';
-
-
-    
-    const handleOkrsSpecificUpdate = (newspecificResult: SpecificOKRsArea["okrsSpecific"]) => {
-    
-        setSpecificOKRsAreaComplete({
-            ...specificOKRsAreaComplete,
-            okrsSpecific:newspecificResult
-        });
-        // console.log(newspecificResult)
-      };
-      const showForm2 = () => {
-        setViewForm(true);
-        setSpecificOKRsAreaComplete({
-          ...specificOKRsAreaComplete,
-          okrSpecificName: specificResultName
-        });
-       
 };
 
-    const handleChangeAdd=() =>{
-      setCompleteOKR(prevCompleteOKR => {
-        return {
-          ...prevCompleteOKR,
-          areas: [
+
+
+    const handleOkrsSpecificUpdate = (newspecificResult: SpecificOKRsArea["okrsSpecific"]) => {
+
+        setSpecificOKRsAreaComplete({
+          okrSpecificName:specificResultName,
+          okrsSpecific:newspecificResult
+      });
+      
+    
+    };
+    
+    const showForm2 = () => {
+      setViewForm(true);
+    };
+
+      
+      const handleChangeArrayArea=()=>{
+      
+      setArrayAreas(() => {
+     
+        const areaExists = arrayAreas?.some((area) => area.name === lastAreaName)
+
+        if (!areaExists) {
+          arrayAreas.push({
+            name:lastAreaName,
+            specificOKRsArea:arraySpecificOKRsArea
+          });
+          if (arrayAreas[0].name === "") {
+            arrayAreas.shift();
+          }
+
+          return arrayAreas;
+        } else {
+          swal("Esta área ya existe");
+          return arrayAreas; 
+        }
+      });
+
+      setSpecificOKRsAreaComplete({
+        okrSpecificName:"",
+        okrsSpecific:[]
+      });
+
+      swal(t("Área añadida con éxito"));
+      }
+
+      
+      const handleChangeAdd=() =>{
+      
+        setCompleteOKR(
+          prevCompleteOKR => {
+            return {
+              ...prevCompleteOKR,
+              areas:arrayAreas
+            };
+          }
+          ); 
+        }
+        
+   
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      // const validationError = validations(completeOKR as ResourceProps);
+      // setErrors(validationError);
+      
+      try {
+          const endPoint = "https://linkit-server.onrender.com/OKRs/create";
+          const response = await axios.post(endPoint, completeOKR, {
+            headers: { Authorization: `Bearer ${token}`,
+            'Accept-Language': sessionStorage.getItem('lang')}
+          });
+          
+          setCompleteOKR(
             {
-              name: areaName,
-              specificOKRsArea: [specificOKRsAreaComplete]
-            }
-          ]
-        };
-      }); 
-    }
-    // console.log(specificOKRsAreaComplete)
+              generalTitleOKR:"",
+              areas:[
+                {
+                      name:"",
+                      specificOKRsArea:[
+                          {
+                          okrSpecificName:"",
+                          okrsSpecific:[]
+                          }
+                      ]
+                  }
+              ],
+              archived:false
+          });
+  
+          swal(t("El OKR fue creado con éxito"));
+          onClose()
+  
+          return response.data;
+        } catch (error: any) {
+          console.error(error.response.data)
+        }
+      };
+    
+// console.log(completeOKR)
+// console.log(arrayAreas)
+// console.log(arraySpecificOKRsArea)
     return(
-      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50">
-            <div className=" flex flex-col justify-center items-center bg-linkIt-500 opa m-32 rounded-[20px] border-[3px] border-linkIt-300 ">
+      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-y-auto">
+            <div className=" flex flex-col justify-center items-center bg-linkIt-500 opa m-32 rounded-[20px] border-[3px] border-linkIt-300">
                 <div>
                     <h1 className="text-3xl my-12">{t("Nuevo OKR")}</h1>
                 </div>
@@ -237,7 +302,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
                                   {completeOKR &&
                                     completeOKR.generalTitleOKR &&
-                                    completeOKR.generalTitleOKR.length > 0 ? (
+                                    completeOKR.generalTitleOKR.length > 0 && completeOKR.generalTitleOKR !== " " ? (
                                       <div className="mx-4">
                                       <h3 className="text-md font-bold text-linkIt-200">
                                         {t('Nombre OKR principal agregado')}
@@ -259,26 +324,35 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
                                           <option value="Recruiting">Recruiting</option>
                                       </select>
 
-                                     
+                                      <button 
+                                
+                                className="bg-linkIt-300 flex justify-center items-center rounded-[7px] mb-12 ml-6 p-6 h-12 w-32 text-white text-[10px] xl:text-xl shadow-md hover:bg-transparent hover:border-linkIt-300 hover:text-black hover:shadow-sm hover:shadow-linkIt-300 transition-all duration-300 ease-in-out active:scale-90"
+                                type="button"
+                                onClick={ handleChangeArrayArea}>
+                          
+                                {t('Agregar área')}
+                                
+                                </button>
                                 
                                     </div>
                                   ) : null}
                                   
-                                <button 
-                                // onClick={handleClick}
-                                className="bg-linkIt-300 flex justify-center items-center rounded-[7px] mb-12 ml-6 p-6 h-12 w-32 text-white text-[10px] xl:text-xl shadow-md hover:bg-transparent hover:border-linkIt-300 hover:text-black hover:shadow-sm hover:shadow-linkIt-300 transition-all duration-300 ease-in-out active:scale-90"
-                                onClick={handleChangeAdd}>
-                                {t('Agregar')}
-                                
-                                </button>
+                              
                             </div>
 
 
                         </div>
+                        <button 
+                                className="bg-linkIt-300 flex justify-center items-center rounded-[7px] mb-12 ml-6 p-6 h-12 w-32 text-white text-[10px] xl:text-xl shadow-md hover:bg-transparent hover:border-linkIt-300 hover:text-black hover:shadow-sm hover:shadow-linkIt-300 transition-all duration-300 ease-in-out active:scale-90"
+                                type="submit"
+                                onClick={handleChangeAdd}>
+                                {t('Crear OKR')}
+                                
+                                </button>
                 </form>
 
             {showForm && (
-              <div className=" flex flex-col justify-center items-center bg-linkIt-500 opa m-32 rounded-[20px] border-[3px] border-linkIt-300 "
+              <div className=" flex flex-col justify-center items-center bg-linkIt-500 opa m-32 rounded-[20px] border-[3px] border-linkIt-300  "
               >
       
               <label className="block uppercase tracking-wide text-black text-xs font-bold mb-2">{t("Resultados clave")}</label>
@@ -290,19 +364,22 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
                                   onChange={handleResultNameChange}
                                   // onBlur={handleBlurErrors}
                                   /> 
-              {viewForm && (
-              <FormSpecificResults
-              onClose={noShowForm}
-              token={token}
-              onOkrsSpecificUpdate={handleOkrsSpecificUpdate}
-              />
-              )}
-
               <button
               className="flex items-center border border-linkIt-300 rounded-[7px] p-2 shadow-md hover:border-linkIt-200 transition-all duration-300 ease-in-out mr-5"
-              onClick={showForm2} //Setea antes de crear los resultados específicos
+              type="button"
+              onClick={showForm2} 
               >Crear Resultados específicos
               </button>
+
+              {viewForm && (
+              <FormSpecificResults
+                onOkrsSpecificUpdate={handleOkrsSpecificUpdate}
+                onClose={() => {
+                  noShowForm();
+                }}
+                token={token}
+              />
+              )}
             </div>)
           }
             </div>
