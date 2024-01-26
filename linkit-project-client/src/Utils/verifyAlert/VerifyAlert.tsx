59ea4961b56a8a5b501c6a2056fddbfb91f05923
Juan Velez <juanvelez.dev@gmail.com>
2024-01-26 18:05:33 -0500

    fixed bug

--- linkit-project-client/src/Utils/verifyAlert/VerifyAlert.tsx
+++ linkit-project-client/src/Utils/verifyAlert/VerifyAlert.tsx
@@ -1,23 +1,20 @@
 import Swal from "sweetalert2"
-import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next"
-import { WebsiteUser } from "../../components/Profiles/types";
 import { useEffect } from "react";
 
 
 export default function VerifyAlert() {
   const { t } = useTranslation();
-  const user: WebsiteUser = useSelector(
-    (state: any) => state.Authentication.user
-  );
   useEffect(() => {
     Swal.fire({
-      title: t("Bienvenido de vuelta"),
-      text: t(`Tu email ${user.email} ha sido verificado, ya puedes iniciar sesión`),
+      title: t("¡Estás listo para empezar!"),
+      text: t(`Tu correo electrónico ha sido verificado con éxito Te invitamos a iniciar sesión y explorar nuestra página. Esperamos que disfrutes de la experiencia`),
       icon: "success",
       iconColor: "#173951",
       background: "#ECEEF0",
       allowOutsideClick: true,
+      confirmButtonColor: "#01A28B",
+      confirmButtonText: t("Continuar"),
     })
   },[])
 
