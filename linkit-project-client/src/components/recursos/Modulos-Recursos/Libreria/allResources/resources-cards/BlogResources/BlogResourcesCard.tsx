752effcf7f7b7ee52810a685e9bb7250175b40c2
Samuel Rodríguez <samuelmateo986@gmail.com>
2024-01-29 07:46:26 -0500

    Style blog´s view and library

--- linkit-project-client/src/components/recursos/Modulos-Recursos/Libreria/allResources/resources-cards/BlogResources/BlogResourcesCard.tsx
+++ linkit-project-client/src/components/recursos/Modulos-Recursos/Libreria/allResources/resources-cards/BlogResources/BlogResourcesCard.tsx
@@ -42,7 +42,7 @@ function BlogsResourceCard({ image, title, description, genre, _id }: BlogsCardP
   const navigate = useNavigate();
   return (
     <motion.div
-      className="flex flex-col border-[0.13rem] w-full rounded-[0.625rem] font-montserrat text-[1.5rem] h-screen xl:h-[30rem] lg:h-[70vh] items-center justify-center bg-white card-container-resources container-resources"
+      className="border-[2px] w-full h-full rounded-xl font-montserrat bg-white dark:border-linkIt-400"
       variants={blogsCardVariants}
       initial="hidden"
       whileInView={"visible"}
@@ -53,24 +53,24 @@ function BlogsResourceCard({ image, title, description, genre, _id }: BlogsCardP
         <img
           src={`https://res.cloudinary.com/dquhriqz3/image/upload/${image}`}
           alt={title}
-          className={`w-full h-[12rem] rounded-[0.625rem] ${title.length > 20 ? "xl:-mt-4" : ""
-            } image`}
+          className={`w-[20rem]  rounded-lg  xs:h-[10rem] ssm:h-[15rem] md:h-[20rem] lg:h-[10rem] xl:h-[12rem] 2xl:h-[16rem] bg-cover bg-center`}
         />
       )}
-      <div className="p-[3rem] flex flex-col flex-grow justify-between lg:w-full lg:p-[1rem] xl:p-[1.8rem] content-container-resources">
-        <div>
-          <p className="border-[2px] text-[0.8rem] mb-[10px] xl:mb-[1.5rem] h-[25px] border-linkIt-300 rounded-[10px] p-[0.8rem] font-semibold items-center justify-center whitespace-nowrap inline-flex lg:text-[0.7rem] genre-resources">
-            {genre}
-          </p>
-          <h1 className="font-bold w-full text-[1.3rem] lg:text-[1rem] xl:w-[17rem] title-resources">
-            {title}
-          </h1>
-          <p className="font-semibold text-[0.9rem] w-full mt-[15px] xl:mb-[1rem] description-resources">
-            {description}
-          </p>
-        </div>
+      <div className="grid grid-rows-4 items-center justify-items-start gap-[5%] h-[16rem] ssm:h-[23rem] md:h-[26rem] lg:h-[20rem] xl:h-[27rem] 2xl:h-[24rem] p-[7%]">
+
+        <p className="border-[1px] text-[0.5rem] xs:text-[0.6rem] ssm:text-[0.8rem] md:text-[1rem] lg:text-[0.8rem] h-fit border-linkIt-300 rounded-[7px] p-1 mb-2 xs:mb-3 font-semibold justify-items-center">
+          {genre}
+        </p>
+        <h1 className="font-bold subtitles-size line-clamp-3">
+          {title}
+        </h1>
+
+        <p className="font-semibold text-size text-ellipsis overflow-clip line-clamp-3 pt-10">
+          {description}
+        </p>
+
         <motion.a
-          className="text-[1rem] font-bold link-resources"
+          className="text-[0.5rem] xs:text-[0.6rem] ssm:text-[0.8rem] md:text-[1rem] font-bold mt-2 xs:mt-3 place-self-end justify-self-start hover:cursor-pointer"
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 1 }}
           onClick={() => { navigate(`/blog/${_id}`) }}
