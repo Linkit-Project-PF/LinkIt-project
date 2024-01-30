945304239402ecaff6d93fffe01ca49482c1bf6e
MateoGuerreroE <mateof1223@gmail.com>
2024-01-29 22:23:06 -0500

    Library responsivity fixed

--- linkit-project-client/src/components/recursos/Modulos-Recursos/Libreria/allResources/resources-cards/BlogResources/BlogResourcesCard.tsx
+++ linkit-project-client/src/components/recursos/Modulos-Recursos/Libreria/allResources/resources-cards/BlogResources/BlogResourcesCard.tsx
@@ -33,11 +33,16 @@ const blogsCardVariants: Variants = {
       delay: 0.2,
       type: "spring",
     },
-  }
+  },
+};
 
-}
-
-function BlogsResourceCard({ image, title, description, genre, _id }: BlogsCardProps) {
+function BlogsResourceCard({
+  image,
+  title,
+  description,
+  genre,
+  _id,
+}: BlogsCardProps) {
   const { t } = useTranslation();
   const navigate = useNavigate();
   return (
@@ -57,13 +62,10 @@ function BlogsResourceCard({ image, title, description, genre, _id }: BlogsCardP
         />
       )}
       <div className="grid grid-rows-4 items-center justify-items-start gap-[5%] h-[16rem] ssm:h-[23rem] md:h-[26rem] lg:h-[20rem] xl:h-[27rem] 2xl:h-[24rem] p-[7%]">
-
         <p className="border-[1px] text-[0.5rem] xs:text-[0.6rem] ssm:text-[0.8rem] md:text-[1rem] lg:text-[0.8rem] h-fit border-linkIt-300 rounded-[7px] p-1 mb-2 xs:mb-3 font-semibold justify-items-center">
           {genre}
         </p>
-        <h1 className="font-bold subtitles-size line-clamp-3">
-          {title}
-        </h1>
+        <h1 className="font-bold subtitles-size line-clamp-3">{title}</h1>
 
         <p className="font-semibold text-size text-ellipsis overflow-clip line-clamp-3 pt-10">
           {description}
@@ -73,9 +75,11 @@ function BlogsResourceCard({ image, title, description, genre, _id }: BlogsCardP
           className="text-[0.5rem] xs:text-[0.6rem] ssm:text-[0.8rem] md:text-[1rem] font-bold mt-2 xs:mt-3 place-self-end justify-self-start hover:cursor-pointer"
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 1 }}
-          onClick={() => { navigate(`/blog/${_id}`) }}
+          onClick={() => {
+            navigate(`/blog/${_id}`);
+          }}
         >
-          {t('Leer Nota')}
+          {t("Leer Nota")}
         </motion.a>
       </div>
     </motion.div>
