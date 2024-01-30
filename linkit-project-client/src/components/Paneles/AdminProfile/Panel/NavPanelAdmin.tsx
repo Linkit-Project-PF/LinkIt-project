dfac5db4f3fe71d6e3edd9f808e57d52b141d4c0
MateoGuerreroE <mateof1223@gmail.com>
2024-01-29 22:02:16 -0500

    Visual int on profile/dashboard fixed

--- linkit-project-client/src/components/Paneles/AdminProfile/Panel/NavPanelAdmin.tsx
+++ linkit-project-client/src/components/Paneles/AdminProfile/Panel/NavPanelAdmin.tsx
@@ -10,91 +10,91 @@ export default function NavPanelAdmin() {
     <nav>
       <ul className="flex flex-row p-6 pl-16 pb-0">
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[0] === 1 ? "bg-linkIt-500" : ""
+          className={`text-2xl mr-16 p-5 ${
+            active[0] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([1, 0, 0, 0, 0, 0, 0])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/statistics/OKRs"
           >
             {t("Estadísticas")}
           </NavLink>
         </li>
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[1] === 1 ? "bg-linkIt-500" : ""
+          className={`text-2xl mr-16 p-5 ${
+            active[1] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([0, 1, 0, 0, 0, 0, 0])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/vacantes"
           >
             {t("Vacantes")}
           </NavLink>
         </li>
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[2] === 1 ? "bg-linkIt-500" : ""
+          className={`text-2xl mr-16 p-5 ${
+            active[2] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([0, 0, 1, 0, 0, 0, 0])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/clientsfollowup"
           >
             {t("Seguimiento")}
           </NavLink>
         </li>
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[3] === 1 ? "bg-linkIt-500" : ""
+          className={`ttext-2xl mr-16 p-5 ${
+            active[3] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([0, 0, 0, 1, 0, 0, 0])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/recursos"
           >
             {t("Recursos")}
           </NavLink>
         </li>
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[4] === 1 ? "bg-linkIt-500" : ""
+          className={`text-2xl mr-16 p-5 ${
+            active[4] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([0, 0, 0, 0, 1, 0, 0])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/reviews"
           >
             {t("Reseñas")}
           </NavLink>
         </li>
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[5] === 1 ? "bg-linkIt-500" : ""
+          className={`text-2xl mr-16 p-5 ${
+            active[5] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([0, 0, 0, 0, 0, 1, 0])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/users"
           >
             {t("Usuarios")}
           </NavLink>
         </li>
         <li
-          className={`text-2xl mr-16 p-2 rounded-t-md ${
-            active[6] === 1 ? "bg-linkIt-500" : ""
+          className={`text-2xl mr-16 p-5 ${
+            active[6] === 1 ? "text-linkIt-300" : ""
           }`}
         >
           <NavLink
             onClick={() => isActive([0, 0, 0, 0, 0, 0, 1])}
-            className="hover:text-linkIt-300"
+            className="hover:text-linkIt-300 text-2xl hover:cursor-pointer font-bold"
             to="/AdminDashboard/mis-datos"
           >
             {t("Mis datos")}
