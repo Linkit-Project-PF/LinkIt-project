2c76e1bd9f91766f27cff720bb110ad0862cf9d2
Santiago Acuña <95108004+Santiago-Acuna@users.noreply.github.com>
2024-04-08 12:06:43 -0300

    Delete linkit-project-client/src/env.ts

--- linkit-project-client/src/env.ts
+++
@@ -1,7 +0,0 @@
-// SIMULATION OF ENV meanwhile just for It to work.
-
-export const SUPERADMN_ID = "6564e8c0e53b0475ffe277f2";
-export const CLOUDINARY_ID = "dquhriqz3";
-
-// LATEST 65566e201b4939c1cef34a54
-// "6564e8c0e53b0475ffe277f2"
