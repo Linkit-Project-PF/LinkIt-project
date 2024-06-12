4a0aa688a36729de895fe4fe2e528acdb5bed1a6
Santiago Acuña <santfloo7xcrl8@gmail.com>
2024-06-12 16:04:35 -0300

    fix de cambios inesperados

--- linkit-project-client/src/components/Talentos/ModulosTalentos/ModuloTalentosA/ModuloTalentosA.tsx
+++ linkit-project-client/src/components/Talentos/ModulosTalentos/ModuloTalentosA/ModuloTalentosA.tsx
@@ -17,14 +17,14 @@ export default function ModuloTalentosA() {
           {t("Conéctate con los mejores proyectos IT")}
         </h2>
       </div>
-      <div className="grid grid-cols-2 h-fit w-screen overflow-hidden bg-linkIt-500 dark:bg-linkIt-200 dark:text-white md:pt-[19vh] ">
-        <div className="sm:mt-[5%] md:space-y-[5%]  pl-[6%] min-[300px]:ml-[7px] min-[350px]:ml-2 min-[450px]:ml-3 min-[500px]:ml-3 ssm:ml-4">
+      <div className="grid grid-cols-2 h-fit w-screen overflow-hidden bg-linkIt-500 dark:bg-linkIt-200 dark:text-white md:pt-[17vh] lg:pt-[19vh] ">
+        <div className="sm:mt-[5%] md:mt-0 md:space-y-[5%] md:p-[9%]  pl-[6%] min-[300px]:ml-[7px] min-[350px]:ml-2 min-[450px]:ml-3 min-[500px]:ml-3 ssm:ml-4 md:ml-0 ">
           <h2 className="hidden md:block font-bold font-manrope xs:text-[1.0rem] ssm:text-[1.8rem] sm:text-[2rem] md:text-[2.6rem] w-[150%] ssm:w-[130%] sm:w-[115%] lg:w-[130%] lg:text-[3rem] xl:text-[3.5rem] xl:w-[112%] 2xl:text-[5rem] leading-tight ">
             {t("Conéctate con los mejores proyectos IT")}
           </h2>
-          <h3 className="hidden md:block font-monserrat text-[0.8rem] lg:text-[1.5rem] md:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2.5rem] w-[110%] ssm:w-[89%] sm:w-[120%]  lg:w-[90%] xl:w-[85%] leading-tight">
+          <h3 className="hidden md:block font-monserrat text-[0.8rem] lg:text-[1.5rem] md:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2.5rem] w-[110%] ssm:w-[89%] sm:w-[75%]  lg:w-[90%] xl:w-[85%] leading-tight">
             {t("Aplica a las mejores oportunidades")} <br />{" "}
-            {t("laborales de manera remota.")}
+            {t("de manera remota.")}
           </h3>
           <h3 className="md:hidden font-monserrat text-[0.4rem] xs:text-[0.5rem] min-[340px]:text-[0.6rem] min-[370px]:text-[0.7rem] min-[400px]:text-[0.8rem] min-[430px]:text-[0.9rem] min-[465px]:text-[1rem] min-[500px]:text-[1.1rem] ssm:text-[1.2rem] min-[600px]:text-[1.3rem] sm:text-[1.4rem] min-[300px]:mt-[10%] ssm:mt-[10%] sm:mt-[5%] md:mt-0 min-[700px]:text-[1.5rem] w-[110%] ssm:w-[115%] sm:w-[115%] leading-tight">
             {t("Aplica a oportunidades")}
@@ -41,7 +41,7 @@ export default function ModuloTalentosA() {
               </motion.button>{" "}
             </a>
           </div>
-          <div className="hidden md:block relative flex top-3">
+          <div className="hidden md:flex relative  flex-row top-3">
             <img
               className="w-1/2 lg:w-1/4 "
               src={isDarkMode ? starsWhite : starsBlue}
@@ -60,7 +60,7 @@ export default function ModuloTalentosA() {
           </div>
         </div>
         <img
-          className="2xl:w-[93%] 2xl:h-[100%] 2xl:ml-12 2xl:mt-0 2xl:bottom-0  xl:w-[90%] xl:ml-6 lg:w-[100%] lg:h-[100%] xs:h-[95%] xs:w-[87%] sm:h-[95%] sm:w-[88%] w-full h-auto md:px-4 min-[400px]:ml-1 min-[450px]:ml-2 ssm:ml-3 ssm:h-[95%] ssm:w-[88%] xs:self-end  md:self-auto  ssm md:mt-[15%] xl:mt-6"
+          className="2xl:w-[93%] 2xl:h-[100%] 2xl:ml-12 2xl:mt-0 2xl:bottom-0  xl:w-[90%] xl:ml-6 lg:w-[100%] lg:h-[100%] xs:h-[95%] xs:w-[87%] sm:h-[95%] sm:w-[88%] w-full h-auto md:px-4 min-[400px]:ml-1 min-[450px]:ml-2 ssm:ml-3 ssm:h-[95%] ssm:w-[88%] md:w-full md:h-full xs:self-end  md:self-auto  md:mt-[15%] xl:mt-6"
           src="/Vectores/linkit-slider-talento.png"
           alt="computer"
         />
