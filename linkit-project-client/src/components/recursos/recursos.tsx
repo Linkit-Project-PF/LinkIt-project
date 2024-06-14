cb1b06ec8750a3f656e40c98df87d4d7cd37b72f
Santiago Acuña <santfloo7xcrl8@gmail.com>
2024-06-14 16:46:16 -0300

    fix de algunos errores

--- linkit-project-client/src/components/recursos/recursos.tsx
+++ linkit-project-client/src/components/recursos/recursos.tsx
@@ -41,21 +41,21 @@ export default function Recursos() {
     <div className="overflow-hidden">
       <section id="moduloA">
         <div
-          className="lg:hidden flex justify-center w-screen bg-linkIt-500 dark:bg-linkIt-200 dark:text-white md:pt-[22vh]"
+          className="md:hidden flex justify-center w-screen bg-linkIt-500 dark:bg-linkIt-200 dark:text-white md:pt-[22vh]"
           style={
             windowWidth <= 767
               ? { marginTop: `${fixedHeight}px`, paddingTop: fixedHeight / 4 }
               : undefined
           }
         >
-          <h2 className=" font-bold font-manrope xs:text-[1rem] min-[350px]:text-[1.1rem] min-[380px]:text-[1.2rem] min-[410px]:text-[1.3rem] min-[435px]:text-[1.4rem] min-[460px]:text-[1.5rem] min-[500px]:text-[1.6rem] ssm:text-[1.8rem] min-[600px]:text-[1.9rem] sm:text-[2.2rem] min-[670px]:text-[2.3rem] md:text-[2.7rem] min-[850px]:text-[2.9rem] min-[900px]:text-[3.1rem] min-[950px]:text-[3.2rem] min-[1000px]:text-[3.4rem] w-[90%] leading-tight ">
-            {t("Impulsa tu carreraen IT con los ")} <br />
+          <h2 className=" font-bold font-manrope md:hidden xs:text-[1rem] min-[350px]:text-[1.1rem] min-[380px]:text-[1.2rem] min-[410px]:text-[1.3rem] min-[435px]:text-[1.4rem] min-[460px]:text-[1.5rem] min-[500px]:text-[1.6rem] ssm:text-[1.8rem] min-[600px]:text-[1.9rem] sm:text-[2.2rem] min-[670px]:text-[2.3rem] md:text-[2.7rem] min-[850px]:text-[2.9rem] min-[900px]:text-[3.1rem] min-[950px]:text-[3.2rem] min-[1000px]:text-[3.4rem] w-[90%] leading-tight ">
+            {t("Impulsa tu carrera en IT con los ")} <br />
             {t("mejores tips y recursos!")}
           </h2>
         </div>
-        <div className="grid grid-cols-2 h-fit md:h-max w-screen overflow-hidden bg-linkIt-500 dark:bg-linkIt-200 dark:text-white lg:pt-[22vh]">
+        <div className="grid grid-cols-2 h-fit md:h-max w-screen overflow-hidden bg-linkIt-500 dark:bg-linkIt-200 dark:text-white   min-[768px]:pt-[22vh] lg:pt-[22vh]">
           <div className="space-y-[5%] p-[7%] pl-[6%] min-[300px]:ml-[8px] min-[350px]:ml-[9px] min-[400px]:ml-[10px] min-[450px]:ml-3 min-[500px]:ml-3 ssm:ml-4 md:ml-4 ">
-            <h2 className="hidden lg:block font-bold font-manrope  w-[150%] lg:w-[130%] lg:text-[3rem] xl:text-[3.5rem] xl:w-[112%] 2xl:text-[5rem] leading-tight ">
+            <h2 className="hidden md:block font-bold font-manrope  md:text-[2.3rem] w-[150%] ssm:w-[130%] sm:w-[115%] lg:w-[130%] lg:text-[3rem] xl:text-[3.5rem] xl:w-[112%] 2xl:text-[5rem] leading-tight">
               {t("Impulsa tu carrera")} {t("en IT con los mejores")}{" "}
               {t("tips y recursos!")}
             </h2>
@@ -68,9 +68,9 @@ export default function Recursos() {
               {t("Blogs, ebooks y webinars")} <br />
               {t("para crecer como profesional")} <br />{t("en el mundo tech.")}{" "}
             </h3>
-            <div className="relative top-[5%] ">
+            <div className="relative top-[5%]  ">
               <motion.a
-                className="background-button xs:text-[0.5rem] min-[400px]:text-[0.6rem] min-[450px]:text-[0.7rem]  min-[500px]:text-[0.8rem] ssm:text-[0.9rem] min-[600px]:text-[1rem] sm:text-[1.1rem] min-[700px]:text-[1.3rem] min-[900px]:text-[1.4rem] "
+                className=" md:mb-20 lg:mb-0 background-button xs:text-[0.5rem] min-[400px]:text-[0.6rem] min-[450px]:text-[0.7rem]  min-[500px]:text-[0.8rem] ssm:text-[0.9rem] min-[600px]:text-[1rem] sm:text-[1.1rem] min-[700px]:text-[1.3rem] min-[900px]:text-[1.4rem] "
                 whileTap={{ scale: 0.9 }}
                 href="https://airtable.com/appPc8zZP29ez9V2O/shrX7MQRZlgmqP6bq"
               >
@@ -79,7 +79,7 @@ export default function Recursos() {
             </div>
           </div>
           <img
-            className="relative self-start content-end w-[90%] left-[15%] xs:left-[6%] xs:w-[80%] xs:mt-4 ssm:w-[80%] ssm:bottom-0 ssm:top[5%] ssm:left-[8%] sm:mt-6 md:content-end  md:w-[80%] md:mt-10 lg:self-end lg:top-4 lg:w-[85%] lg:left-[7%]  xl:-mt-4 xl:w-[83%] 2xl:h-[100%]"
+            className="relative self-start content-end w-[90%] left-[15%] xs:left-[6%] xs:w-[80%] xs:mt-4 ssm:w-[80%] ssm:bottom-0 ssm:top[5%] ssm:left-[8%] sm:mt-6 md:self-end  md:w-[80%] md:mt-10 md:mb-20 lg:mb-0 lg:self-end lg:top-4 lg:w-[85%] lg:left-[7%]  xl:-mt-4 xl:w-[83%] 2xl:h-[100%]"
             src="/Vectores/linkit-slider-soy-recursos.svg"
             alt="Businessman"
           />
