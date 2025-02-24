d66c9cd43f79b6e90cffbda0452f48480606f82b
Linkit-Project-PF <150060814+Linkit-Project-PF@users.noreply.github.com>
2025-02-24 17:00:44 -0300

    Ft/no sitemap public (#570)
    * No more static sitemap
    
    * try for deploy
    
    ---------
    
    Co-authored-by: juanmeyer94 <juan_meyer@hotmail.com.ar>

--- linkit-project-client/vite.config.ts
+++ linkit-project-client/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/sitemap.xml': {
+        target: 'https://linkit-server.onrender.com',
+        changeOrigin: true
+      }
+    }
+  },
+  preview: {
     proxy: {
       '/sitemap.xml': {
         target: 'https://linkit-server.onrender.com',
