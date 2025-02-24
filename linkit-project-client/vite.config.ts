29767526d17daba4e8ed028e564fddd909596c0d
juanmeyer94 <juan_meyer@hotmail.com.ar>
2025-02-24 16:51:01 -0300

    No more static sitemap

--- linkit-project-client/vite.config.ts
+++ linkit-project-client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/sitemap.xml': {
+        target: 'https://linkit-server.onrender.com',
+        changeOrigin: true
+      }
+    }
+  }
 })
