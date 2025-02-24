37e138a43c4e277391ee2fbbd40974ce6c773422
juanmeyer94 <juan_meyer@hotmail.com.ar>
2025-02-24 15:54:01 -0300

    Friendly Urls

--- linkit-project-client/vite.config.ts
+++ linkit-project-client/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["pdfjs-dist/build/pdf.worker.min.js"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          pdfWorker: ["pdfjs-dist/build/pdf.worker.min.js"],
+        },
+      },
+    },
+  },
 })
+
