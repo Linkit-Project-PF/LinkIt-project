eac8f38baa68eb3bb0c476af6b2d33d7ca688686
juanmeyer94 <juan_meyer@hotmail.com.ar>
2025-07-15 13:06:19 -0300

    Language Back

--- linkit-project-client/src/Utils/Language.tsx
+++ linkit-project-client/src/Utils/Language.tsx
@@ -3,19 +3,7 @@ import "./language.css"
 import { useEffect } from "react"
 
 export default function Language() {
-  const { i18n } = useTranslation("global")
-
-  useEffect(() => {
-    const storedLang = sessionStorage.getItem('lang');
-    if (!storedLang) {
-      const browserLang = navigator.language.startsWith("en") ? "en" : "es";
-      i18n.changeLanguage(browserLang);
-      sessionStorage.setItem('lang', browserLang);
-    } else {
-      i18n.changeLanguage(storedLang);
-    }
-  }, []);
-
+  const {i18n} = useTranslation("global")
 
   useEffect(() => {
     sessionStorage.setItem('lang', i18n.language)
@@ -24,19 +12,17 @@ export default function Language() {
   const handleClick = () => {
     i18n.changeLanguage(i18n.language === "es" ? "en" : "es")
   }
-
   return (
     <>
-      <input onChange={handleClick} type="checkbox" id="checkbox" />
-      <label className="switch" htmlFor="checkbox">
-        <div className="en-lang">
-          <p className={i18n.language === 'en' ? 'text-linkIt-300' : 'text-black dark:text-white'}>EN</p>
-        </div>
-        <span className="dark:text-white">|</span>
-        <div className="es-lang">
-          <p className={i18n.language === 'es' ? 'text-linkIt-300' : 'text-black dark:text-white'}>ES</p>
-        </div>
-      </label>
+    <input onChange={handleClick} type="checkbox" id="checkbox" /><label className="switch" htmlFor="checkbox">
+      <div className="en-lang">
+      <p className={i18n.language === 'en' ? 'text-linkIt-300' : 'text-black dark:text-white'}>EN</p>
+      </div>
+      <span className="dark:text-white">|</span>
+      <div className="es-lang">
+      <p className={i18n.language === 'es' ? 'text-linkIt-300' : 'text-black dark:text-white'}>ES</p>
+      </div>
+    </label>
     </>
   )
 }
